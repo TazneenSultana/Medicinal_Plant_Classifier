90b1df652551f3506b21144ce94e5d97add543ee
TazneenSultana <46434900+TazneenSultana@users.noreply.github.com>
2026-05-11 22:19:30 +0600

    Update app.py
    test theme

--- app.py
+++ app.py
@@ -227,13 +227,9 @@ def classify_plant(image: Image.Image):
 # =========================================================
 # 6. GRADIO INTERFACE
 # =========================================================
-# Custom CSS to force green...............
+# ...........................................
 
-with gr.Blocks(theme=gr.themes.Default(
-        primary_hue=gr.themes.colors.green, 
-        secondary_hue=gr.themes.colors.lime,
-        neutral_hue=gr.themes.colors.slate
-    ), title="Medicinal Plant Classifier") as iface:
+with gr.Blocks(theme=gr.themes.Default(primary_hue="red", secondary_hue="pink")) as iface:
     gr.Markdown("# 🌱 Scan Your Medicinal Leaf with AI")
     #gr.Markdown("Upload an image of a medicinal plant leaf. This model was trained for 40 epochs.")
 
